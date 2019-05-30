e0b14bdfa46038fa4bd745d6862f39f5fda52984
Anastasiya Aliashkevich <a.v.aliashkevich@gmail.com>
2019-05-30 18:17:06 +0200

    possible fix for CORS issue

--- index.js
+++ index.js
@@ -11,34 +11,64 @@ const port = process.env.PORT || 3000;
 const api_path = `/api/v1/`;
 
 app.get('/', (req, res) => {
+  res.set({
+    'Access-Control-Allow-Origin':
+      'http://localhost:3000,http://localhost:3001,http://localhost:3002',
+    'Access-Control-Allow-Methods': '*',
+  });
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.get(api_path + 'users', (req, res) => {
+  res.set({
+    'Access-Control-Allow-Origin':
+      'http://localhost:3000,http://localhost:3001,http://localhost:3002',
+    'Access-Control-Allow-Methods': '*',
+  });
   res.json({
     users,
   });
 });
 
 app.get(api_path + 'projects', (req, res) => {
+  res.set({
+    'Access-Control-Allow-Origin':
+      'http://localhost:3000,http://localhost:3001,http://localhost:3002',
+    'Access-Control-Allow-Methods': '*',
+  });
   res.json({
     projects,
   });
 });
 
 app.get(api_path + 'roles', (req, res) => {
+  res.set({
+    'Access-Control-Allow-Origin':
+      'http://localhost:3000,http://localhost:3001,http://localhost:3002',
+    'Access-Control-Allow-Methods': '*',
+  });
   res.json({
     roles,
   });
 });
 
 app.get(api_path + 'tasks', (req, res) => {
+  res.set({
+    'Access-Control-Allow-Origin':
+      'http://localhost:3000,http://localhost:3001,http://localhost:3002',
+    'Access-Control-Allow-Methods': '*',
+  });
   res.json({
     tasks,
   });
 });
 
 app.get(api_path + 'clients', (req, res) => {
+  res.set({
+    'Access-Control-Allow-Origin':
+      'http://localhost:3000,http://localhost:3001,http://localhost:3002',
+    'Access-Control-Allow-Methods': '*',
+  });
   res.json({
     clients,
   });
