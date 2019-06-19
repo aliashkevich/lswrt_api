c088d6425b8b020deeb53b39f382de51b9f8c01d
laura-mso <laura.socha@gmx.net>
2019-06-19 14:48:37 +0200

    Added app.delete access conrol

--- index.js
+++ index.js
@@ -26,6 +26,14 @@ app.get('/*', function(req, res, next) {
   next();
 });
 
+app.delete('/*', function(req, res, next) {
+  res.set({
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': '*',
+  });
+  next();
+});
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
