6f561eff24bf540a3c75ed4eddbfb2545db8a8a0
Sarah Wilsoncook <sarahlouise125@gmail.com>
2019-07-01 15:21:54 +0200

    and updated clients file

--- routes/clients.js
+++ routes/clients.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const clients = require('../data/clients.json');
-const formidable = require('formidable');
 
 router.get('/', function(req, res, next) {
   res.json({
